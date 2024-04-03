3c317caa391234520d75849dc7177e03dae96bf6
Trey <trey.shearin3@gmail.com>
2024-04-02 22:00:58 -0400

    created seed information for Wargear.js

--- models/Wargear.js
+++ models/Wargear.js
@@ -1,2 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
+class Wargear extends Model {}
   
+Wargear.init(
+    {
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        range: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        attack: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        strength: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        armor_pierce: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        damage: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'wargear',
+      }
+);
+
+module.exports = Wargear;
