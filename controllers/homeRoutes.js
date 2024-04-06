bc2ade94a98d01592c3da16e9dc775ad681e8f6e
Trey <trey.shearin3@gmail.com>
2024-04-06 19:33:12 -0400

    got homepage to load using api/army route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,25 +2,24 @@ const router = require('express').Router();
 const { User, Army, Mini, Wargear } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
+router.get('/home', async (req, res) => {
   try {
     // Get all armys and JOIN with user data
-    const armyData = await Army.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
+    // const armyData = await Army.findAll({
+    //   include: [
+    //     {
+    //       model: User,
+    //       attributes: ['name'],
+    //     },
+    //   ],
+    // });
     
     // Serialize data so the template can read it
-    const armys = armyData.map((army) => army.get({ plain: true }));
+    // const armys = armyData.map((army) => army.get({ plain: true }));
 
     // Pass serialized data and session flag into template
     res.render('homepage', { 
-      projects, 
-      logged_in: req.session.logged_in 
+      title: 'Homepage'
     });
   } catch (err) {
     res.status(500).json(err);
